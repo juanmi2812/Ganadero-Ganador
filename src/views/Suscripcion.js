fa478192f0feb0aa233643d1a243915870fa3c94
Juan Miguel Yraidini <juanmiguelyraidini@gmail.com>
2026-06-19 17:22:30 -0600

    IDs de Stripe

--- src/views/Suscripcion.js
+++ src/views/Suscripcion.js
@@ -7,8 +7,8 @@ export default function Suscripcion({ usuario, setUsuario }) {
   const [cargando, setCargando] = useState(false);
 
   // Reemplazar con los IDs reales de los precios de Stripe que creaste
-  const PRICE_ID_MENSUAL = "price_1Sty..._REEMPLAZAME"; 
-  const PRICE_ID_ANUAL = "price_1Sty..._REEMPLAZAME";
+  const PRICE_ID_MENSUAL = "price_1TkB7GGT0cvg4XcjwmpmY7eI"; 
+  const PRICE_ID_ANUAL = "price_1TkB7GGT0cvg4Xcj6Lz7MFcE";
 
   const iniciarCheckout = async (priceId) => {
     setCargando(true);
